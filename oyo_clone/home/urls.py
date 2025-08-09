5130d76696ccf396ea1e527255d20dcb5b70d12d
Horizon-1803 <iamhorizon18@gmail.com>
2025-08-09 15:00:55 +0530

    first commit

---
+++ oyo_clone/home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('',views.index, name="index"),
+    path('login/',views.login_page, name="login"),
+    path('register/',views.register_page, name="register")
+]
